9dd81658129de87db892cdb88d2a7e7cfc755ccd
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2024-03-22 14:11:03 +0600

    Some changes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/user.controllers');
-const { verifyToken } = require('../middlewares/authMiddleware');
+const userController = require("../controllers/user.controllers");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 // Routes for getting all users
-router.get('/', verifyToken, userController.getAllUsers);
+router.get("/", verifyToken, userController.getAllUsers);
 
 // Route for getting a single user by id
-router.get('/:id', userController.getUserById);
+router.get("/:id", userController.getUserById);
 
 // Route for creating a new user
-router.post('/', userController.createUser);
+router.post("/", userController.createUser);
 
 // Route for authentication a user
-router.post('/authenticate', userController.authenticate);
+router.post("/authenticate", userController.authenticate);
 
 // Route for updating a user
-router.put('/:id', userController.updateUser);
+router.put("/:id", userController.updateUser);
 
 // Route for deleting a user
-router.delete('/:id', userController.deleteUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 // imports
-const cors = require('cors')
+const cors = require("cors");
 const express = require("express");
-const env = require('dotenv')
+const env = require("dotenv");
 const connectDB = require("./connection/connectionDB.js");
 
+const dashboardRoutes = require("./routes/dashboard.routes");
 
 env.config();
 const app = express();
@@ -12,13 +13,13 @@ const PORT = process.env.PORT || 5000;
 /* Set Middle wares  */
 app.use(cors());
 app.use(express.json());
- 
+
 // database connect
-connectDB()
+connectDB();
 
 // Application Level  Middleware pattern
 app.use("/api/v1/user", require("./routes/user.routes.js"));
-
+app.use("/api/v1/dashboard", dashboardRoutes);
 
 // Server Start
 app.listen(PORT, () => {
