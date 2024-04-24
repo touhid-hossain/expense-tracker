af1e017cc2f23e4628947aa50a2d92eac8228fc3
unknown <mohmmedtouhid007@gmail.com>
2024-04-24 17:23:06 +0600

    gg

--- server/controllers/transactions.controllers.js
+++ server/controllers/transactions.controllers.js
@@ -74,27 +74,227 @@ exports.getAllTransaction = async (req, res) => {
 };
 
 // Function for get aggregateTransactionList depends on query.
+// exports.getTransactionSummary = async (req, res) => {
+//   const type = req.query?.type;
+//   const time = req.query?.time;
+//   console.log("Getting type and time in backend fnc", type, time);
+
+//   const startOfTime = moment().startOf(time);
+//   const endOfTime = moment().endOf(time);
+
+//   console.log("Checking time formation", startOfTime, endOfTime);
+//   try {
+// Checking the CreatedAt time formation
+// const transactions = await Transaction.find({
+//   createdAt: {
+//     $gte: startOfTime,
+//     $lte: endOfTime,
+//   },
+// });
+
+// console.log("Checking the CreatedAt time formation: ", transactions);
+
+//     const yearlyResult = await Transaction.aggregate([
+//       {
+//         $group: {
+//           _id: {
+//             $year: "$createdAt",
+//           },
+//           items: {
+//             $push: {
+//               type: "$type",
+//               year: {
+//                 $year: "$createdAt",
+//               },
+//               amount: {
+//                 $toInt: "$amount",
+//               },
+//               creator: "$creator",
+//               month: {
+//                 $arrayElemAt: [
+//                   [
+//                     {
+//                       monthName: "Jan",
+//                       monthIndex: 0,
+//                     },
+//                     {
+//                       monthName: "Feb",
+//                       monthIndex: 1,
+//                     },
+//                     {
+//                       monthName: "Mar",
+//                       monthIndex: 2,
+//                     },
+//                     {
+//                       monthName: "Apr",
+//                       monthIndex: 3,
+//                     },
+//                     {
+//                       monthName: "May",
+//                       monthIndex: 4,
+//                     },
+//                     {
+//                       monthName: "Jun",
+//                       monthIndex: 5,
+//                     },
+//                     {
+//                       monthName: "Jul",
+//                       monthIndex: 6,
+//                     },
+//                     {
+//                       monthName: "Aug",
+//                       monthIndex: 7,
+//                     },
+//                     {
+//                       monthName: "Sep",
+//                       monthIndex: 8,
+//                     },
+//                     {
+//                       monthName: "Oct",
+//                       monthIndex: 9,
+//                     },
+//                     {
+//                       monthName: "Nov",
+//                       monthIndex: 10,
+//                     },
+//                     {
+//                       monthName: "Dec",
+//                       monthIndex: 11,
+//                     },
+//                   ],
+//                   {
+//                     $subtract: [{ $month: "$createdAt" }, 1],
+//                   },
+//                 ],
+//               },
+//             },
+//           },
+//         },
+//       },
+//       {
+//         $match: {
+//           _id: new Date().getFullYear(),
+//         },
+//       },
+//       {
+//         $unwind: "$items", // Unwind the items array
+//       },
+//       {
+//         $group: {
+//           _id: "$items.month",
+
+//           // Group by month
+//           items: {
+//             $push: "$items",
+//           }, // Push items into an array for each month
+//         },
+//       },
+//       {
+//         $addFields: {
+//           results: {
+//             $reduce: {
+//               input: "$items",
+//               initialValue: {
+//                 totalIncomeAmount: 0,
+//                 totalExpenseAmount: 0,
+//                 info: {},
+//               },
+//               in: {
+//                 $cond: {
+//                   if: {
+//                     $eq: ["$$this.type", "income"],
+//                   },
+//                   then: {
+//                     $mergeObjects: [
+//                       "$$value",
+//                       {
+//                         info: {
+//                           year: "$$this.year",
+//                           userId: "$$this.creator",
+//                           currency: "BDT",
+//                         },
+//                       },
+//                       {
+//                         totalIncomeAmount: {
+//                           $add: ["$$value.totalIncomeAmount", "$$this.amount"],
+//                         },
+//                       },
+//                     ],
+//                   },
+//                   else: {
+//                     $mergeObjects: [
+//                       "$$value",
+//                       {
+//                         info: {
+//                           year: "$$this.year",
+//                           userId: "$$this.creator",
+//                           currency: "BDT",
+//                         },
+//                       },
+//                       {
+//                         totalExpenseAmount: {
+//                           $add: ["$$value.totalExpenseAmount", "$$this.amount"],
+//                         },
+//                       },
+//                     ],
+//                   },
+//                 },
+//               },
+//             },
+//           },
+//         },
+//       },
+//       {
+//         $project: {
+//           _id: 0,
+//           monthName: "$_id.monthName",
+//           monthIndex: "$_id.monthIndex",
+//           results: 1,
+//         },
+//       },
+//       {
+//         $sort: {
+//           monthIndex: 1,
+//         },
+//       },
+//       {
+//         $replaceRoot: {
+//           newRoot: {
+//             $mergeObjects: [
+//               {
+//                 month: "$monthName",
+//               },
+//               "$results",
+//             ],
+//           },
+//         },
+//       },
+//     ]);
+
+//     console.log("checking aggregate result", yearlyResult);
+//     res.status(200).json({ aggregateValue: yearlyResult });
+
+//     // if (time === "year") {
+//     // } else {
+//     //   console.log("Time Doest match");
+//     // }
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+// }
+
 exports.getTransactionSummary = async (req, res) => {
   const type = req.query?.type;
   const time = req.query?.time;
   console.log("Getting type and time in backend fnc", type, time);
 
-  const startOfTime = moment().startOf(time);
-  const endOfTime = moment().endOf(time);
+  // const startOfTime = moment().startOf(time);
+  // const endOfTime = moment().endOf(time);
 
-  console.log("Checking time formation", startOfTime, endOfTime);
+  // console.log("Checking time formation", startOfTime, endOfTime);
   try {
-    // Checking the CreatedAt time formation
-    // const transactions = await Transaction.find({
-    //   createdAt: {
-    //     $gte: startOfTime,
-    //     $lte: endOfTime,
-    //   },
-    // });
-
-    // console.log("Checking the CreatedAt time formation: ", transactions);
-
-    const yearlyResult = await Transaction.aggregate([
+    const monthlyResult = await Transaction.aggregate([
       {
         $group: {
           _id: {
@@ -106,10 +306,12 @@ exports.getTransactionSummary = async (req, res) => {
               year: {
                 $year: "$createdAt",
               },
+              day: {
+                $dayOfMonth: "$createdAt",
+              },
               amount: {
                 $toInt: "$amount",
               },
-              creator: "$creator",
               month: {
                 $arrayElemAt: [
                   [
@@ -181,7 +383,10 @@ exports.getTransactionSummary = async (req, res) => {
       },
       {
         $group: {
-          _id: "$items.month",
+          _id: {
+            month: "$items.month.monthName",
+            day: "$items.day",
+          },
 
           // Group by month
           items: {
@@ -189,6 +394,7 @@ exports.getTransactionSummary = async (req, res) => {
           }, // Push items into an array for each month
         },
       },
+
       {
         $addFields: {
           results: {
@@ -212,6 +418,7 @@ exports.getTransactionSummary = async (req, res) => {
                           year: "$$this.year",
                           userId: "$$this.creator",
                           currency: "BDT",
+                          day: "$$this.day",
                         },
                       },
                       {
@@ -229,6 +436,7 @@ exports.getTransactionSummary = async (req, res) => {
                           year: "$$this.year",
                           userId: "$$this.creator",
                           currency: "BDT",
+                          day: "$$this.day",
                         },
                       },
                       {
@@ -246,38 +454,25 @@ exports.getTransactionSummary = async (req, res) => {
       },
       {
         $project: {
-          _id: 0,
-          monthName: "$_id.monthName",
-          monthIndex: "$_id.monthIndex",
+          _id: 1,
           results: 1,
         },
       },
       {
-        $sort: {
-          monthIndex: 1,
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: {
-            $mergeObjects: [
-              {
-                month: "$monthName",
-              },
-              "$results",
-            ],
-          },
+        $group: {
+          _id: "$_id.month",
+          items: { $push: "$results" },
         },
       },
     ]);
 
-    console.log("checking aggregate result", yearlyResult);
+    console.log("checking aggregate result", monthlyResult);
 
-    if (time === "year") {
-      return res.status(200).json({ aggregateValue: yearlyResult });
-    } else {
-      console.log("Time Doest match");
-    }
+    res.status(200).json({ aggregateValue: monthlyResult });
+    // if (time === "month") {
+    // } else {
+    //   console.log("Time Doest match");
+    // }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
