884531f6194a11e070dfadf8c477512d518bb4b8
Md Saiful Islam Shanto <mdsaifulislamshanto03@gmail.com>
2024-05-26 21:52:10 +0600

    ready for deploy

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://expense-tracker-kk3n.onrender.com/api/v1",
   withCredentials: false,
 });
 instance.interceptors.request.use(function (config) {
